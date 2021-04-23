f5566f999fb5758f26a7b43f9fd91ed79d01e5c8
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-23 18:34:06 -0400

    altered app.js, index.js and got rid of some things

--- client/src/login.js
+++ client/src/login.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
 const AUTH_URL =
 	'https://accounts.spotify.com/authorize? client_id=49cb9bffd6fd4bb9b6df96a0858a23fa&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 export default function login() {
-	return <div />;
+	return (
+		<Container>
+			<a className="btn btn-success btn-lg" href={AUTH_URL}>
+				Login With Spotify
+			</a>
+		</Container>
+	);
 }
