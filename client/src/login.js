a4a00219994a08da18dfd0ed245684cdbd20611b
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-25 17:43:58 -0400

    fixed login

--- client/src/login.js
+++ client/src/login.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 
 const AUTH_URL =
-	'https://accounts.spotify.com/authorize? client_id=49cb9bffd6fd4bb9b6df96a0858a23fa&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+	'https://accounts.spotify.com/authorize?client_id=49cb9bffd6fd4bb9b6df96a0858a23fa&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 export default function login() {
 	return (
