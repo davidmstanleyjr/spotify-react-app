d28348ca0cdc6260475da4bb36729e6d1e065eff
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-24 20:12:15 -0400

    centered Spotify button

--- client/src/login.js
+++ client/src/login.js
@@ -7,7 +7,7 @@ const AUTH_URL =
 
 export default function login() {
 	return (
-		<Container>
+		<Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
 			<a className="btn btn-success btn-lg" href={AUTH_URL}>
 				Login With Spotify
 			</a>
