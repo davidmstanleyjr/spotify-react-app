6a475d08bbf8d1e394d3c571a08eaab222e33d30
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-27 17:11:20 -0400

    did some work in useAuth.js

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,31 +1,47 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function useAuth(code) {
-	const [ accessToken, setAccessToken ] = useState();
-	const [ refreshToken, setRefreshToken ] = useState();
-	const [ expiresIn, setExpiresIn ] = useState();
+  const [accessToken, setAccessToken] = useState()
+  const [refreshToken, setRefreshToken] = useState()
+  const [expiresIn, setExpiresIn] = useState()
 
-	//this gets all the info we need to make the code above work
-	useEffect(
-		() => {
-			axios
-				.post('http://localhost:3001/login', {
-					code
-				})
-				.then((res) => {
-					setAccessToken(res.data.accessToken);
-					setRefreshToken(res.data.refreshToken);
-					setExpiresIn(res.data.expiresIn);
 
-					window.history.pushState({}, null, '/');
-				})
-				.catch(() => {
-					window.location = '/';
-				});
-		},
-		{ code }
-	);
+  //this gets all the code we need to make the coe above work
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/login", {
+        code,
+      })
+      .then(res => {
+        setAccessToken(res.data.accessToken)
+        setRefreshToken(res.data.refreshToken)
+        setExpiresIn(res.data.expiresIn)
+        window.history.pushState({}, null, "/")
+      })
+      .catch(() => {
+        window.location = "/"
+      })
+  }, [code])
 
-	return accessToken;
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then(res => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+          window.location = "/"
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+  }, [refreshToken, expiresIn])
+
+  return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -3,29 +3,48 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const SpotifyWebApi = require('spotify-web-api-node');
 
-
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/login', function(req, res)  {
-    const code = req.body.code;
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri: 'http://localhost:3000',
-      clientId: '49cb9bffd6fd4bb9b6df96a0858a23fa',
-      clientSecret: '1dfcf0a1642b4a1eae578cf378c872ea'
-    })
+app.post('/refresh', (req, res) => {
+	const refreshToken = req.body.refreshToken;
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: 'http://localhost:3000',
+		clientId: '49cb9bffd6fd4bb9b6df96a0858a23fa',
+		clientSecret: '1dfcf0a1642b4a1eae578cf378c872ea',
+		refreshToken
+	});
+	spotifyApi
+		.refreshAccessToken()
+		.then((data) => {
+			console.log(data.body);
+		})
+		.catch(() => {
+			res.sendStatus(400);
+		});
+});
 
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        });
-    }).catch((err) => {
-        res.sendStatus(400);
-    })
-})
+app.post('/login', function(req, res) {
+	const code = req.body.code;
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: 'http://localhost:3000',
+		clientId: '49cb9bffd6fd4bb9b6df96a0858a23fa',
+		clientSecret: '1dfcf0a1642b4a1eae578cf378c872ea'
+	});
 
-app.listen(3001)
+	spotifyApi
+		.authorizationCodeGrant(code)
+		.then((data) => {
+			res.json({
+				accessToken: data.body.access_token,
+				refreshToken: data.body.refresh_token,
+				expiresIn: data.body.expires_in
+			});
+		})
+		.catch((err) => {
+			res.sendStatus(400);
+		});
+});
+
+app.listen(3001);
