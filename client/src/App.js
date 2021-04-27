f499e6909ec0ffee1157a04cab2ed93f5331aa42
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-27 00:01:05 -0400

    added dashboard js and useAuth js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './login';
+import Dashboard from './dashboard';
+
+//takes in url code so users can login
+const code = new URLSearchParams(window.location.search).get
+('code')
 
 function App() {
-	return <Login />;
+	return code ? <Dashboard code={code} /> : <Login />;
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
 app.post('/login', function(req, res) => {
-    const code = req.body.code
+    const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
       redirectUri: 'http://localhost:3000',
       clientId: '49cb9bffd6fd4bb9b6df96a0858a23fa',
@@ -18,8 +18,10 @@ app.post('/login', function(req, res) => {
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
-        })
+        });
     }).catch(() => {
-        res.sendStatus(400)
+        res.sendStatus(400);
     })
 })
+
+app.listen(3001)
