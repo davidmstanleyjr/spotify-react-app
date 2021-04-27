9e1f1376fcce56d1f025c7e6b6171ecebcfea4c0
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-27 15:53:28 -0400

    refresh token works. URL is also shortened

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,19 +1,31 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'
-
+import axios from 'axios';
 
 export default function useAuth(code) {
-   const [accessToken, setAccessToken] = useState();
-   const [refreshToken, setRefreshToken] = useState();
-   const [expiresIn, setExpiresIn] = useState();
+	const [ accessToken, setAccessToken ] = useState();
+	const [ refreshToken, setRefreshToken ] = useState();
+	const [ expiresIn, setExpiresIn ] = useState();
 
+	//this gets all the info we need to make the code above work
+	useEffect(
+		() => {
+			axios
+				.post('http://localhost:3001/login', {
+					code
+				})
+				.then((res) => {
+					setAccessToken(res.data.accessToken);
+					setRefreshToken(res.data.refreshToken);
+					setExpiresIn(res.data.expiresIn);
 
-   //this gets all the info we need to make the code above work
-   useEffect(() => {
-       axios.post('http://localhost:3001/login', {
-           code,
-       }).then(res => {
-           console.log(res.data)
-       })
-   }, {code});
+					window.history.pushState({}, null, '/');
+				})
+				.catch(() => {
+					window.location = '/';
+				});
+		},
+		{ code }
+	);
+
+	return accessToken;
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/login', function(req, res) => {
+app.post('/login', function(req, res)  {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
       redirectUri: 'http://localhost:3000',
@@ -19,7 +23,7 @@ app.post('/login', function(req, res) => {
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         });
-    }).catch(() => {
+    }).catch((err) => {
         res.sendStatus(400);
     })
 })
