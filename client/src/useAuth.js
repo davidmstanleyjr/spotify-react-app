9cde0e5d46c0d90a2ca97e3343b2d79b31717711
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-18 15:30:40 -0400

    changed some ports in useAuth.js

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,48 +1,55 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function useAuth(code) {
-  const [accessToken, setAccessToken] = useState()
-  const [refreshToken, setRefreshToken] = useState()
-  const [expiresIn, setExpiresIn] = useState()
+	const [ accessToken, setAccessToken ] = useState();
+	const [ refreshToken, setRefreshToken ] = useState();
+	const [ expiresIn, setExpiresIn ] = useState();
 
+	//this gets all the code we need to make the coe above work
+	useEffect(
+		() => {
+			axios
+				.post('http://localhost:5000/login', {
+					// .post("http://localhost:3001/login", {
+					code
+				})
+				.then((res) => {
+					setAccessToken(res.data.accessToken);
+					setRefreshToken(res.data.refreshToken);
+					setExpiresIn(res.data.expiresIn);
+					window.history.pushState({}, null, '/');
+				})
+				.catch(() => {
+					window.location = '/';
+				});
+		},
+		[ code ]
+	);
 
-  //this gets all the code we need to make the coe above work
-  useEffect(() => {
-    axios
-      .post("http://localhost:3001/login", {
-        code,
-      })
-      .then(res => {
-        setAccessToken(res.data.accessToken)
-        setRefreshToken(res.data.refreshToken)
-        setExpiresIn(res.data.expiresIn)
-        window.history.pushState({}, null, "/")
-      })
-      .catch(() => {
-        window.location = "/"
-      })
-  }, [code])
+	useEffect(
+		() => {
+			if (!refreshToken || !expiresIn) return;
+			const interval = setInterval(() => {
+				axios
+					.post('http://localhost:5000/refresh', {
+						// .post("http://localhost:3001/refresh", {
+						refreshToken
+					})
+					.then((res) => {
+						setAccessToken(res.data.accessToken);
+						setExpiresIn(res.data.expiresIn);
+					})
+					// a failure will redirect the user to the homepage
+					.catch(() => {
+						window.location = '/';
+					});
+			}, (expiresIn - 60) * 1000);
 
-  useEffect(() => {
-    if (!refreshToken || !expiresIn) return;
-    const interval = setInterval(() => {
-      axios
-        .post("http://localhost:3001/refresh", {
-          refreshToken,
-        })
-        .then(res => {
-          setAccessToken(res.data.accessToken)
-          setExpiresIn(res.data.expiresIn)
-        })
-		// a failure will redirect the user to the homepage
-        .catch(() => {
-          window.location = "/";
-        })
-    }, (expiresIn - 60) * 1000);
+			return () => clearInterval(interval);
+		},
+		[ refreshToken, expiresIn ]
+	);
 
-    return () => clearInterval(interval);
-  }, [refreshToken, expiresIn]);
-
-  return accessToken;
+	return accessToken;
 }
