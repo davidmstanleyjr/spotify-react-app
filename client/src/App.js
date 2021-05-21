58a8fa9760a96ec7ae11c41d5c2bff5f82085595
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-21 19:08:51 -0400

    added some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './login';
 import Dashboard from './dashboard';
+import './Style.css';
 
 //takes in url code so users can login
-const code = new URLSearchParams(window.location.search).get
-('code');
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
 	return code ? <Dashboard code={code} /> : <Login />;
